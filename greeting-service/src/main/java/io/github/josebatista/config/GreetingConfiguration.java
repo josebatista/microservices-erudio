bc42b09049932efca8cc540aeaeda789a79473d3
Jose Pereira <josebatistapereira@gmail.com>
2025-09-14 08:17:29 -0300

    Atualiza configuracoes sem interromper o servico

--- greeting-service/src/main/java/io/github/josebatista/config/GreetingConfiguration.java
+++ greeting-service/src/main/java/io/github/josebatista/config/GreetingConfiguration.java
@@ -1,7 +1,27 @@
 package io.github.josebatista.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @ConfigurationProperties("greeting-service")
-public record GreetingConfiguration(String greeting, String defaultValue) {
+@RefreshScope // Usado para atualizar as configuracoes apos alteracao do config server, chamar o actuator/refresh (POST)
+public class GreetingConfiguration {
+    private String greeting;
+    private String defaultValue;
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+
+    public String getDefaultValue() {
+        return defaultValue;
+    }
+
+    public void setDefaultValue(String defaultValue) {
+        this.defaultValue = defaultValue;
+    }
 }
