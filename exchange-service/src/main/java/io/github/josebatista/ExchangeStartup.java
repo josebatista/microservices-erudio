a04b067261f0726afcd9f46de86985f94ad20c20
Jose Pereira <josebatistapereira@gmail.com>
2025-09-14 08:37:02 -0300

    Cria ExchangeService

---
+++ exchange-service/src/main/java/io/github/josebatista/ExchangeStartup.java
@@ -0,0 +1,13 @@
+package io.github.josebatista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeStartup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeStartup.class, args);
+	}
+
+}
---
+++ exchange-service/src/test/java/io/github/josebatista/ExchangeStartupTests.java
@@ -0,0 +1,13 @@
+package io.github.josebatista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeStartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
