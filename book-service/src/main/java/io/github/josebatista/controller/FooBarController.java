433b693d8e25a015d378d18cb4acc1d3e2f542c6
Jose Pereira <josebatistapereira@gmail.com>
2025-09-14 20:49:03 -0300

    Adiciona Rate Limiter e BulkHead

--- book-service/src/main/java/io/github/josebatista/controller/FooBarController.java
+++ book-service/src/main/java/io/github/josebatista/controller/FooBarController.java
@@ -1,13 +1,12 @@
 package io.github.josebatista.controller;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/book-service")
@@ -18,12 +17,15 @@ public class FooBarController {
     @GetMapping("/foo-bar")
 //    @Retry(name = "default") // 3 tentativas
 //    @Retry(name = "foo-bar", fallbackMethod = "fallbackMethod") // configurado no application.yaml
-    @CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
+//    @CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
+    @RateLimiter(name = "default")
+    @Bulkhead(name = "default")
     public String fooBar() {
-        logger.info("Request to foo-bar is received!");
-        ResponseEntity<String> response = new RestTemplate()
-                .getForEntity("http://localhost:8080/foo-bar", String.class);
-        return response.getBody();
+//        logger.info("Request to foo-bar is received!");
+//        ResponseEntity<String> response = new RestTemplate()
+//                .getForEntity("http://localhost:8080/foo-bar", String.class);
+//        return response.getBody();
+        return "Foo bar!!";
     }
 
     private String fallbackMethod(Exception ex) {
