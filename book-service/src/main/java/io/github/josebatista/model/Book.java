aa062bdfb854469bf5a3d22ba5ddeef120d88580
Jose Pereira <josebatistapereira@gmail.com>
2025-09-14 11:39:15 -0300

    Mapeia entidade, implementa repository

--- book-service/src/main/java/io/github/josebatista/model/Book.java
+++ book-service/src/main/java/io/github/josebatista/model/Book.java
@@ -1,20 +1,45 @@
 package io.github.josebatista.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.persistence.Transient;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
+@Entity(name = "book")
 public class Book implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "author", nullable = false, length = 180)
     private String author;
+
+    @Column(name = "title", nullable = false, length = 250)
     private String title;
+
+    @Column(name = "launch_date", nullable = false)
+    @Temporal(TemporalType.DATE)
     private Date launchDate;
+
+    @Column(name = "price", nullable = false)
     private Double price;
+
+    @Transient
     private String currency;
+
+    @Transient
     private String environment;
 
     public Book() {
---
+++ book-service/src/main/java/io/github/josebatista/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package io.github.josebatista.repository;
+
+import io.github.josebatista.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
