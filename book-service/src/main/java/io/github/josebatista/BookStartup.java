a597aa06fa4a006385c001e6a319dde5782ca7d2
Jose Pereira <josebatistapereira@gmail.com>
2025-09-14 11:08:19 -0300

    Cria BookService

---
+++ book-service/src/main/java/io/github/josebatista/BookStartup.java
@@ -0,0 +1,13 @@
+package io.github.josebatista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStartup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStartup.class, args);
+	}
+
+}
