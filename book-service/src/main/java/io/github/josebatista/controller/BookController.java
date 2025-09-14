cf82ffb7f5e6e94f03986147d956841f9551ec67
Jose Pereira <josebatistapereira@gmail.com>
2025-09-14 12:06:47 -0300

    Utilizando o FeignClient para comunicar com o ExchangeService

--- book-service/src/main/java/io/github/josebatista/controller/BookController.java
+++ book-service/src/main/java/io/github/josebatista/controller/BookController.java
@@ -3,17 +3,14 @@ package io.github.josebatista.controller;
 import io.github.josebatista.dto.ExchangeDto;
 import io.github.josebatista.environment.InstanceInformationService;
 import io.github.josebatista.model.Book;
+import io.github.josebatista.proxy.ExchangeProxy;
 import io.github.josebatista.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
 
 @RestController
 @RequestMapping("/book-service")
@@ -25,6 +22,9 @@ public class BookController {
     @Autowired
     private BookRepository repository;
 
+    @Autowired
+    private ExchangeProxy proxy;
+
     // http://localhost:8100/book-service/1/BRL
     @GetMapping(value = "/{id}/{currency}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Book getBook(
@@ -32,20 +32,10 @@ public class BookController {
             @PathVariable("currency") String currency
     ) {
         Book book = repository.findById(id).orElseThrow();
-        HashMap<String, String> params = new HashMap<>();
-        params.put("amount", book.getPrice().toString());
-        params.put("from", "USD");
-        params.put("to", currency);
-        ResponseEntity<ExchangeDto> response = new RestTemplate()
-                .getForEntity(
-                        "http://localhost:8000/exchange-service/{amount}/{from}/{to}",
-                        ExchangeDto.class,
-                        params
-                );
-        ExchangeDto exchange = response.getBody();
+        ExchangeDto exchange = proxy.getExchange(book.getPrice(), "USD", currency);
         book.setPrice(exchange.getConvertedValue());
         book.setCurrency(currency);
-        book.setEnvironment("PORT: " + informationService.retrieveServerPort());
+        book.setEnvironment("PORT: " + informationService.retrieveServerPort() + " FEIGN");
         return book;
     }
 
---
+++ book-service/src/main/java/io/github/josebatista/proxy/ExchangeProxy.java
@@ -0,0 +1,18 @@
+package io.github.josebatista.proxy;
+
+import io.github.josebatista.dto.ExchangeDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "exchange-service", url = "localhost:8000")
+public interface ExchangeProxy {
+
+    @GetMapping(value = "/exchange-service/{amount}/{from}/{to}")
+    ExchangeDto getExchange(
+            @PathVariable("amount") Double amount,
+            @PathVariable("from") String from,
+            @PathVariable("to") String to
+    );
+
+}
