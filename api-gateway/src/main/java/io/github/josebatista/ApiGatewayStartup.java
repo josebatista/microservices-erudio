20ea69e2a6b41e494c3988041261f38c7df1499c
Jose Pereira <josebatistapereira@gmail.com>
2025-09-14 18:49:04 -0300

    Cria ApiGateway

---
+++ api-gateway/src/main/java/io/github/josebatista/ApiGatewayStartup.java
@@ -0,0 +1,13 @@
+package io.github.josebatista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayStartup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayStartup.class, args);
+	}
+
+}
