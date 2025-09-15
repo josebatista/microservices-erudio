3c8b03305d50dbcf0e92fbaa31adf52dd3b82b85
Jose Pereira <josebatistapereira@gmail.com>
2025-09-15 20:38:38 -0300

    Configura SpringDoc OpenAPI UI (Swagger)

--- exchange-service/src/main/java/io/github/josebatista/controller/ExchangeController.java
+++ exchange-service/src/main/java/io/github/josebatista/controller/ExchangeController.java
@@ -3,6 +3,8 @@ package io.github.josebatista.controller;
 import io.github.josebatista.environment.InstanceInformationService;
 import io.github.josebatista.model.Exchange;
 import io.github.josebatista.repository.ExchangeRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 
+@Tag(name = "Exchange Endpoint")
 @RestController
 @RequestMapping("exchange-service")
 public class ExchangeController {
@@ -23,6 +26,7 @@ public class ExchangeController {
     private ExchangeRepository exchangeRepository;
 
     // http://localhost:8000/exchange-service/5/USD/BRL
+    @Operation(summary = "Get an exchange from amount of currency!")
     @GetMapping(value = "/{amount}/{from}/{to}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Exchange getExchange(
             @PathVariable("amount") BigDecimal amount,
